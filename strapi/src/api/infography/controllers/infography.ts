13b9e04c75b02f43bb1f83a9afa300cc7be63772
Juandiego001 <juandiego14012003@gmail.com>
2025-04-06 22:01:07 -0500

    feat/strapi - add strapi to start deployment

---
+++ strapi/src/api/infography/controllers/infography.ts
@@ -0,0 +1,7 @@
+/**
+ * infography controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::infography.infography');
---
+++ strapi/src/api/infography/routes/infography.ts
@@ -0,0 +1,7 @@
+/**
+ * infography router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::infography.infography');
